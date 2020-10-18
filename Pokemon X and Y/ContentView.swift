5e2e223604d3749535536ccd40afaa69b781396d
as468579 <as468579@gmail.com>
2020-10-18 15:25:25 +0800

    Initial Commit

---
+++ Pokemon X and Y/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokemon X and Y
+//
+//  Created by User18 on 2020/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pokemon X and Y/Pokemon_X_and_YApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_X_and_YApp.swift
+//  Pokemon X and Y
+//
+//  Created by User18 on 2020/10/18.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_X_and_YApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
